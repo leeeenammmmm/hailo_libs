5ca40d12dc5b6cb37da4e717ad47c28c3cb57121
leeenammmmm <lethanhnam12a1ltt@gmail.com>
2024-04-12 13:07:24 +0300

    re-id

--- postprocesses/re-id/re-id_overlay.cpp
+++ postprocesses/re-id/re-id_overlay.cpp
@@ -95,6 +95,45 @@ static void printImageDimensions(const cv::Mat& image) {
     std::cout << ")" << std::endl;
 }
 
+static cv::Mat bgrToNV12(const cv::Mat& bgrImage) {
+    // Step 1: Split BGR image into channels
+    cv::Mat channels[3];
+    cv::split(bgrImage, channels);
+
+    // Step 2: Convert to grayscale (Y plane)
+    cv::Mat yPlane;
+    cv::cvtColor(bgrImage, yPlane, cv::COLOR_BGR2GRAY);
+
+    // Step 3: Resize and combine blue and red channels to form UV plane
+    cv::Mat uvPlane;
+    cv::resize(channels[0], channels[0], cv::Size(), 0.5, 0.5); // Downsample blue channel
+    cv::resize(channels[2], channels[2], cv::Size(), 0.5, 0.5); // Downsample red channel
+    std::vector<cv::Mat> uvChannels = {channels[0], channels[2]};
+    cv::merge(uvChannels, uvPlane); // Combine blue and red channels
+    std::cout << "Bug here" << std::endl;
+    // Step 4: Concatenate Y and UV planes to form NV12 image
+    std::vector<cv::Mat> nv12Planes = {yPlane, uvPlane};
+    cv::Mat nv12Image;
+    cv::merge(nv12Planes, nv12Image);
+
+    return nv12Image;
+}
+
+static cv::Mat BGR2YUV_NV12(const cv::Mat &src) {
+  auto src_h = src.rows, src_w = src.cols;
+  cv::Mat dst(src_h * 1.5, src_w, CV_8UC1);
+  cv::cvtColor(src, dst, cv::COLOR_BGR2YUV_I420);
+  auto n_y = src_h * src_w;
+  auto n_uv = n_y / 2, n_u = n_y / 4;
+  std::vector<uint8_t> uv(n_uv);
+  std::copy(dst.data+n_y, dst.data+n_y+n_uv, uv.data());
+  for (auto i = 0; i < n_u; i++) {
+    dst.data[n_y + 2*i] = uv[i];            // U
+    dst.data[n_y + 2*i + 1] = uv[n_u + i];  // V
+  }
+  return dst;
+}
+
 static void draw_detection(cv::Mat &image_planes, HailoDetectionPtr detection, HailoROIPtr roi, int font_thickness = 1, int line_thickness = 1)
 {
 
@@ -108,13 +147,10 @@ static void draw_detection(cv::Mat &image_planes, HailoDetectionPtr detection, H
                               ((detection_bbox.ymax() * roi_bbox.height()) + roi_bbox.ymin()) * image_planes.rows);
     auto bbox_width = bbox_max.x - bbox_min.x;
 
-    std::cout << "------Ok-----------";
-
     // Draw the detection box
     auto color_rgb = (!global_id) ? DEFAULT_DETECTION_COLOR : indexToColor(global_id->get_id());
     cv::rectangle(image_planes, bbox_min, bbox_max, color_rgb, line_thickness);
 
-    std::cout << "---------Not Ok----------";
 
     if (global_id)
     {
@@ -126,8 +162,31 @@ static void draw_detection(cv::Mat &image_planes, HailoDetectionPtr detection, H
         
         // Draw the class and confidence text
         cv::putText(image_planes, id_text, text_position, cv::FONT_HERSHEY_SIMPLEX, font_scale, color_rgb, font_thickness);
-        std::cout << "-------------" << id_text << "---------------------";
+        std::cout << "Detect an old person has id: " << id_text << std::endl;
     }
+    else{
+        std::cout << "Detect a new person!" << std::endl;
+    }
+}
+
+static void copyNv12DataToGstVideoFrame(const cv::Mat& nv12Image, GstVideoFrame* frame) {
+    // Assuming nv12Image is a cv::Mat representing an NV12 image
+
+    // Map the GstVideoFrame to access its data pointers
+    GstMapInfo mapInfo;
+    gst_buffer_map(frame->buffer, &mapInfo, GST_MAP_WRITE);
+
+    // Copy NV12 data to the GstVideoFrame
+    guint8* data = mapInfo.data;
+
+    // Copy Y plane data
+    memcpy(data, nv12Image.data, nv12Image.rows * nv12Image.cols);
+
+    // Copy UV plane data
+    memcpy(data + nv12Image.rows * nv12Image.cols, nv12Image.data + nv12Image.rows * nv12Image.cols, nv12Image.rows * nv12Image.cols / 2);
+
+    // Unmap the frame
+    gst_buffer_unmap(frame->buffer, &mapInfo);
 }
 
 void filter(HailoROIPtr roi, GstVideoFrame *frame, gchar *current_stream_id)
@@ -159,26 +218,19 @@ void filter(HailoROIPtr roi, GstVideoFrame *frame, gchar *current_stream_id)
     int uvStride = GST_VIDEO_FRAME_PLANE_STRIDE(frame, 1);
 
     // Create an OpenCV Mat for Y channel
-    std::cout << "before yMat" << std::endl;
     cv::Mat yMat(height, width, CV_8UC1, yData, yStride);
 
     // Create an OpenCV Mat for UV channels
-    std::cout << "before uvMat" << std::endl;
     cv::Mat uvMat(height / 2, width / 2, CV_8UC2, uvData, uvStride);
 
-    std::cout << "after" << std::endl;
     // Convert NV12 to BGR
     cv::Mat mat;
     cv::cvtColorTwoPlane(yMat, uvMat, mat, cv::COLOR_YUV2BGR_NV12);
 
-    std::cout << "-------" << mat.channels() << "-----------" << std::endl;
 
     if (mat.channels() != 3) {
         throw std::runtime_error("Invalid number of channels in converted image");
     }
-
-    printImageDimensions(mat);
-
 
     for (auto obj : roi->get_objects())
     {
@@ -186,9 +238,9 @@ void filter(HailoROIPtr roi, GstVideoFrame *frame, gchar *current_stream_id)
         {
         case HAILO_DETECTION:
         {
-            std::cout << "------------detection-----------------";
             HailoDetectionPtr detection = std::dynamic_pointer_cast<HailoDetection>(obj);
             draw_detection(mat, detection, roi, font_thickness, line_thickness);
+
             break;
         }
         default:
@@ -197,6 +249,10 @@ void filter(HailoROIPtr roi, GstVideoFrame *frame, gchar *current_stream_id)
             break;
         }
     }
+
+    mat = BGR2YUV_NV12(mat);
+
+    copyNv12DataToGstVideoFrame(mat, frame);
 
     mat.release();
 }
